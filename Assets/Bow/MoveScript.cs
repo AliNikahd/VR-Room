019f402fba8a1e6f65bb84366330170b8ad46739
kodabdan <144759976+kodabdan@users.noreply.github.com>
2025-04-09 18:00:23 -0500

    Added Bow to Assets, Made Archery Range

---
+++ Assets/Bow/MoveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+
+    public float frequency;
+    public float x;
+    public float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(x*Mathf.Sin(2 * Mathf.PI * frequency * Time.time), 0, z);
+    }
+}
